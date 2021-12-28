0ed4113f9e7fba8b235cd72fdfc1d96f36cb117c
Ricardo Ribeiro <ricardoribeiro.j7@gmail.com>
2021-12-28 16:31:52 -0300

    implemented promotion card.

---
+++ src/Components/Promotion/Card/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Card.css";
+
+export default function PromotionCard({promotion}){
+    return(
+        <div className="promotion-card">
+            <img src={promotion.imageUrl} alt="imagem do produto." className="promotion-card__image"/>
+            <div className="promotion-card__info">
+                <h1 className="promotion-card__title">{promotion.title}</h1>
+                <span className="promotion-card__price">R$ {promotion.price}</span>
+                <footer className="promotion-card__footer">
+                    {promotion.comments.length > 0 &&(
+                        <div className="promotion-card__comment">"{promotion.comments[0].comment}"</div>
+                    )}
+
+                    <div className="promotion-card__comments-count">{promotion.comments.length} {promotion.comments.length > 1 ? "Comentários" :"Comentário"}</div>
+                    <a href={promotion.url} target="_blank" rel="noreferrer" className="promotion-card__link">IR PARA O SITE</a>
+                </footer>
+            </div>
+        </div>
+    )
+}
