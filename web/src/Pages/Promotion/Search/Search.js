cc22512fc3ac9c2099e78b1c62d99c1cc88878fa
Ricardo Ribeiro <ricardoribeiro.j7@gmail.com>
2021-12-29 11:17:56 -0300

    feature-4: implemented api rest with json-server.

--- web/src/Pages/Promotion/Search/Search.js
+++ web/src/Pages/Promotion/Search/Search.js
@@ -1,24 +1,23 @@
-//import react from "react";
+import react, {useEffect, useState} from "react";
+import axios from "axios";
 import PromotionCard from "../../../Components/Promotion/Card/Card";
 
-const promotion = {
-    "id": 1,
-    "title": "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
-    "price": 1799,
-    "imageUrl": "https://m.media-amazon.com/images/I/61o0lt6Sh6L._AC_SL1000_.jpg",
-    "url": "https://www.amazon.com.br/Notebook-Acer-Mochila-A315-41-R790-Mem%C3%B3ria/dp/B07YDWLV7S/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=gatry0b-20&linkId=e4a1146599e36741a720a6a952cbc328&language=pt_BR",
-    "comments": [
-      {
-        "id": 1,
-        "comment": "TELA HD"
-      }
-    ]
-}
-
 export default function PagesPromotionSearch(){
-    return(
-        <div style={{maxWidth: 800, margin: '30px auto'}}>
+
+  const [promotions, setPromotions] = useState([]);
+
+  useEffect(() =>{
+    axios.get('http://localhost:5000/promotions?_embed=comments')
+      .then((response) => {
+        setPromotions(response.data)
+      });
+  }, []);//Passando o array vazio o useEffect será executado somente quando o componente for montado.
+
+  return(
+      <div style={{maxWidth: 800, margin: '30px auto'}}>
+          {promotions.map((promotion) => (
             <PromotionCard promotion={promotion}/>
-        </div>
-    );
+          ))}
+      </div>
+  );
 }
